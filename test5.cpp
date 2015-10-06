de977d3bf721a6495e3b13050e79e43235dc1126
juedaiyuer <337189277@qq.com>
2015-10-06 17:14:38 +0800

    修改了注释
    注释remove_if（）注释修改

--- test5.cpp
+++ test5.cpp
@@ -36,8 +36,12 @@ int main()
 	cout << "The sum is " << result.sum << endl;
 
 	 vector<int>::iterator new_end =   
-	 remove_if(V.begin(), V.end(), bind2nd(greater<int>(), 100));  //remove_if是否能够在条件函数那里添加bind2nd(less<int>(), 1000)，这个问题没有解决哦！！！
-     V.erase(new_end, V.end()); 
+	 remove_if(V.begin(), V.end(), bind2nd(greater<int>(), 100));  /*1.remove_if是否能够在条件函数那里添加bind2nd(less<int>(), 1000)，这个问题没有解决哦！！！
+																	 2.remove_if只是改变了容器里面元素的位置，完成清除操作还需要调用erase( )
+																   */
+     
+	 
+	 V.erase(new_end, V.end()); 
 
 	 for(int j=0;j<V.size();j++)
 	 {
