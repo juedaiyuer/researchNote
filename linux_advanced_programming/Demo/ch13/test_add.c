9b30885b543b4eea7015ca42b2267add9c643208
zhanhunyiran <2860015667@qq.com>
2016-03-25 21:10:58 +0800

    添加注释

--- linux_advanced_programming/Demo/ch13/test_add.c
+++ linux_advanced_programming/Demo/ch13/test_add.c
@@ -5,14 +5,14 @@
 #include <sys/socket.h>
 int main(int argc,char *argv[])
 {
-	in_addr_t net;
-	struct in_addr net_addr,ret;
+	in_addr_t net; //32位IP地址
+	struct in_addr net_addr,ret; 
 	char buf[128];	
 	memset(buf,'\0',128);
-	net=inet_addr("192.168.68.128");
+	net=inet_addr("192.168.68.128");//转换成为32位IP地址
 	net_addr.s_addr=net;
 	
-	printf("inet_addr(192.168.68.128)=0x%x\n",inet_addr("192.168.68.128"));
+	printf("inet_addr(192.168.68.128)=0x%x\n",inet_addr("192.168.68.128")); //以十六进制输出无符号整数,不输出0x
 	printf("inet_network(192.168.68.128)=0x%x\n",inet_network("192.168.68.128"));
 	
 	printf("inet_ntoa(net)%s\n",inet_ntoa(net_addr));
