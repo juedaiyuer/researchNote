207ad311477862a7a122b0abb36316a568fbb436
zhanhunyiran <2860015667@qq.com>
2016-03-28 10:32:19 +0800

    文件修改

--- linux_advanced_programming/Demo/ch08/getpid_example.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-#include<unistd.h>
-int main(int argc,char *argv[])
-{
-	printf("the current program's pid is %d\n",getpid());
-	return 0;
-}
--- linux_advanced_programming/Demo/ch08/getppid_example.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-#include<unistd.h>
-int main(int argc,char *argv[])
-{
-	printf("the current program's ppid is %d\n",getppid());
-	return 0;
-}
